52d2f44772ed244158a64615f39012f51592f851
Gjin Rexhaj <gjinrexhaj1@gmail.com>
2024-06-01 21:14:54 -0400

    Removed unnecessary imports

--- SierpinskiRev/src/Model.java
+++ SierpinskiRev/src/Model.java
@@ -1,13 +1,7 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
 
 import javax.swing.*;
-import java.io.File;
 
-
-import javax.imageio.ImageIO;
 
 
 @SuppressWarnings("serial")
