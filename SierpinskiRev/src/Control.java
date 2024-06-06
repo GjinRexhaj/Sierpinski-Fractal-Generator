5133e3123f5cb6b1ebceb7144812a9a300e3f5e2
Gjin Rexhaj <gjinrexhaj1@gmail.com>
2024-06-06 17:48:44 -0400

    Remove "suppress warning" annotation

--- SierpinskiRev/src/Control.java
+++ SierpinskiRev/src/Control.java
@@ -12,7 +12,6 @@ import java.awt.event.MouseWheelEvent;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 
-@SuppressWarnings("serial")
 public class Control extends JFrame implements MouseListener, MouseWheelListener, MouseMotionListener {
   
   // static iterator variable for save image name incrementing
